8ce6ecff92fe77edf617fa965697806b1ef6d346
Hyoung Sub Kim <show9114@gmail.com>
2016-07-21 14:56:26 +0900

    add libraries

--- app/src/main/java/kr/pm10/networksample/ui/MainActivity.java
+++ app/src/main/java/kr/pm10/networksample/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package kr.pm10.networksample;
+package kr.pm10.networksample.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.pm10.networksample.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
